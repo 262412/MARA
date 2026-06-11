a95423143aed48ad6bba002884d4cce0d7335507
chenghao zhang <2282610045@qq.com>
2026-06-11 20:38:53 +0100

    file style check

--- flowsettings.py
+++ flowsettings.py
@@ -5,7 +5,9 @@ from ktem.runtime_defaults import build_kotaemon_settings
 from theflow.settings.default import *  # noqa
 
 this_dir = Path(__file__).resolve().parent
-app_data_dir = Path(os.environ.get("KH_APP_DATA_DIR", this_dir / "ktem_app_data")).expanduser()
+app_data_dir = Path(
+    os.environ.get("KH_APP_DATA_DIR", this_dir / "ktem_app_data")
+                    ).expanduser()
 
 globals().update(
     build_kotaemon_settings(
